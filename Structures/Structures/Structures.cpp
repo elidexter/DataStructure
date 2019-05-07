58803eaaaaf95739277a2a5b3ab1032a402fe1d9
elidexter <elidexter@gmail.com>
2019-05-06 20:06:09 -0600

    firsth prototype

---
+++ Structures/Structures/Structures.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+struct Node
+{
+	int data;
+	Node* next;
+};
+void Insert(Node** head, int x)
+{
+	struct Node* temp = (Node*)malloc(sizeof(struct Node));
+	temp->data = x;
+	temp->next = NULL;
+	if (*head != NULL)
+	{
+		temp->next = *head;
+	}
+	*head = temp;		
+}
+void Print(Node* head)
+{
+	printf("List is: ");
+	while (head != NULL)
+	{
+		printf(" %d", head->data);
+		head = head->next;
+	}
+	printf("\n");
+}
+int main()
+{    
+	Node* head = NULL;
+	printf("How many numbers?\n");
+	int n, i, x;
+	scanf_s("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		printf("Enter the number \n");
+		scanf_s("%d", &x);
+		Insert(&head, x);
+		Print(head);
+	}
+}
