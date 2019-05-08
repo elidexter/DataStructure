01d4c2423d1f4bd6d06893a05a1510527b0b0f9b
elidexter <elidexter@gmail.com>
2019-05-07 23:16:06 -0600

    Menu add.. linkedlist customize

--- Structures/Structures/linkedlist.cpp
+++ Structures/Structures/linkedlist.cpp
@@ -6,36 +6,6 @@ struct Node
 	Node* next;
 };
 struct Node* head;
-void InsertAtEnd(int data)
-{
-	struct Node* temp = (Node*)malloc(sizeof(struct Node));
-	temp->data = data;
-	temp->next = NULL;
-	if (head != NULL)
-	{
-		temp->next = head;
-	}
-	head = temp;
-}
-void InsertAt()
-{
-	int val, pos,idx;
-	printf("Node position to insert:\n");
-	scanf_s("%d", &pos);
-	printf("Node value to insert:\n");
-	scanf_s("%d", &val);
-	struct Node* newnode = (Node*)malloc(sizeof(struct Node));
-	newnode->data = val;
-	newnode->next = NULL;
-	Node* temp = head;
-	for (idx = 0; idx < pos - 2; idx++)//get node before selected node
-	{
-		temp = temp->next;
-	}
-	newnode->next = temp->next;
-	temp->next = newnode;
-	printf("Current position before: %d\n", temp->data);
-}
 void Print()
 {
 	Node* temp = head;
@@ -49,6 +19,80 @@ void Print()
 	}
 	printf("\n");
 }
+void InsertAtEnd()
+{
+	int val = 0;
+	printf("Node value to insert:\n");
+	scanf_s("%d", &val);
+	struct Node* temp = (Node*)malloc(sizeof(struct Node));
+	temp->data = val;
+	temp->next = NULL;
+	if (head != NULL)
+	{
+		temp->next = head;
+	}
+	head = temp;
+}
+void fill(int data)
+{
+	struct Node* temp = (Node*)malloc(sizeof(struct Node));
+	temp->data = data;
+	temp->next = NULL;
+	if (head != NULL)
+	{
+		temp->next = head;
+	}
+	head = temp;
+}
+void InsertAtFirst()
+{
+	int val = 0;
+	printf("Node value to insert:\n");
+	scanf_s("%d", &val);
+	struct Node* temp = (Node*)malloc(sizeof(struct Node));
+	temp->data = val;
+	temp->next = NULL;
+	if (head == NULL)
+	{
+		head=temp;
+	}
+	else
+	{
+		temp->next = head;
+		head = temp;
+	}
+	Print();
+}
+void InsertAtPosition()
+{
+	int val=0, pos,idx;
+	printf("Node position to insert:\n");
+	scanf_s("%d", &pos);
+	printf("Node value to insert:\n");
+	scanf_s("%d", &val);
+	struct Node* newnode = (Node*)malloc(sizeof(struct Node));
+	newnode->data = val;
+	newnode->next = NULL;
+	Node* temp = head;
+	for (idx = 0; idx < pos - 2; idx++)//get node before selected node
+	{
+		if (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		else
+		{
+			printf("Out index\n");
+			return;
+		}
+	}
+	if (temp != NULL)
+	{
+		newnode->next = temp->next;
+		temp->next = newnode;
+	}	
+}
+
 void DeleteFirst()//equivale a eliminar el head y poner el siguiente
 {
 	if (head == NULL)
@@ -59,6 +103,34 @@ void DeleteFirst()//equivale a eliminar el head y poner el siguiente
 	Node* tempntext = head;
 	head = tempntext->next;
 	free(tempntext);
+}
+void DeleteLast()
+{
+	if (head == NULL)
+	{
+		printf("LinkedList Empty\n");
+		return;
+	}
+	if (head->next == NULL)
+	{
+		head = NULL;
+		return;
+	}
+	Node* currentNode = head;	
+	Node* previosnode = NULL;
+	while (currentNode->next != NULL)
+	{
+		previosnode = currentNode;
+		if (previosnode->next->next == NULL)
+		{
+			currentNode = currentNode->next;			
+			break;
+		}
+		currentNode = currentNode->next;
+	}	
+	previosnode->next = NULL;
+	free(currentNode);
+	Print();
 }
 void DeleteAt()
 {
@@ -92,15 +164,18 @@ void main()
 	int i,d,e;
 	for (i = 5; i> 0; i--)
 	{
-		InsertAtEnd(i);
+		fill(i);
 	}	
 	Print();
 	do
 	{
-		printf("1.Delete firsth\n");
-		printf("2.Delete at position\n");
-		printf("3.Insert at position\n");
-		printf("4.Print List\n");
+		printf("1.Delete first\n");
+		printf("2.Delete Last\n");
+		printf("3.Delete at position\n");
+		printf("4.Insert at position\n");
+		printf("5.Insert first\n");
+		printf("6.Insert Last\n");
+		printf("7.Print List\n");
 		printf("10.Exit\n");
 		scanf_s("%d", &e);
 		switch (e)
@@ -110,14 +185,20 @@ void main()
 				Print();
 				break;
 			case 2:
+				DeleteLast();
+				break;
+			case 3:
 				DeleteAt();
 				Print();
 				break;
-			case 3:
-				InsertAt();
+			case 4:
+				InsertAtPosition();
 				Print();
 				break;
-			case 4:
+			case 5:
+				InsertAtFirst();
+				break;
+			case 7:
 				Print();
 				break;
 			default:
